b7a944b8d7ffa2795f64b057ac56e6acaedee5c6
Hiroki1238 <c14h14n3nao3s@gmail.com>
2022-11-25 20:52:23 +0900

    確認

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Index = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Index
