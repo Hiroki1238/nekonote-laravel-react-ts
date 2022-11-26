ca0a8f3f45f6271667a513a8658eecb416260046
Yota310 <jingangsihaiuei@gmail.com>
2022-11-26 19:05:42 +0900

    確認

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+    DB::table('users')->insert([
+'last_name'=>'猫山',
+'first_name'=>'ネコ',
+'user_name'=>'いぬ',
+'email'=>'aaa@gmail.com',
+'password'=>'aaiiuueeoo',
+'image_path'=>null,
+'vehicle_id'=>null,
+    ]);
+    
     }
 }
+
+
+
+
+$table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('user_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('image_path')->nullable();
+            $table->foreignId('vehicle_id')->nullable()->constrained('vehicles');
