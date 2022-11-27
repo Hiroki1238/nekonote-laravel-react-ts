3748b24825d7fe528622e5afc09966d50bb7376d
Yota310 <jingangsihaiuei@gmail.com>
2022-11-27 13:04:27 +0900

    [add]Mypagcontrollerの追加、デザインの修正

---
+++ app/Http/Controllers/MypageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\{User};
+
+
+
+
+class MypageController extends Controller
+{
+    public function index(User $user)
+    {
+        return Inertia::render("Mypage/Index", ['user' => $user]);
+    }
+}
